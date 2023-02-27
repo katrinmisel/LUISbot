7c06ab644a9b8b8c178468218c065570cb4e99f7
Katrin <katrinmisel@gmail.com>
2023-02-27 13:39:28 +0100

    secret

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class DefaultConfig:
     #APP_ID = ""
     #APP_PASSWORD = ""
     APP_ID = os.environ.get("MicrosoftAppId", "792b65c2-8a6d-4332-adbb-683221ab6e93")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "4TZ8Q~ORsqvoc4M5D.69QfcOy9iWtaRdI4iSWb9b")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "a3W8Q~UeA7Kflkmy7lLtIYcg.qQaa4ARW676PapT")
     LUIS_APP_ID = os.environ.get("LuisAppId", "9a2e59b9-686d-492b-959b-8ceff62b8494")
     LUIS_API_KEY = os.environ.get("LuisAPIKey", "31031bdf48c943c5a836b722abe99440")
     # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
