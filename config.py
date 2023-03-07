d7b4626c094c293b52ff2e310b11fb289132bdf0
Katrin <katrinmisel@gmail.com>
2023-03-07 19:18:17 +0100

    updates

--- config.py
+++ config.py
@@ -9,12 +9,9 @@ class DefaultConfig:
     """Configuration for the bot."""
 
     PORT = 8000
-    APP_ID = os.environ.get("MicrosoftAppId", "8395514b-224f-435e-944d-134b00913f0d")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "hFW8Q~kzQnV1EPtOxi4km_MaRO7wTybDohsiwaO5")
-    LUIS_APP_ID = os.environ.get("LuisAppId", "db70b5ef-3103-46a0-aa92-7cbfd39c1439")
-    LUIS_API_KEY = os.environ.get("LuisAPIKey", "86630a0d4ffa4c56a2a80714dce8714c")
-    # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
-    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "https://luisp10.cognitiveservices.azure.com/")
-    APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get(
-        "AppInsightsInstrumentationKey", "61449954-e098-49e5-b651-4d70250e38d8"
-    )
+    APP_ID = os.environ.get('APP_ID')
+    APP_PASSWORD = os.environ.get('APP_PASSWORD')
+    LUIS_APP_ID = os.environ.get('LUIS_APP_ID')
+    LUIS_API_KEY = os.environ.get('LUIS_API_KEY')
+    LUIS_API_HOST_NAME = os.environ.get('LUIS_API_HOST_NAME')
+    APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get('APPINSIGHTS_INSTRUMENTATION_KEY')
